2d06ec061bb897eff8754149be2f45f4877563ba
Kane <dmccer@gmail.com>
2015-08-19 21:59:10 +0800

    server

---
+++ built/app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Kane on 15/8/19.
+ */
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var api = require('./api');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/report', api);
+
+module.exports = app;
+
+var server = app.listen(9875, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.info('报告数据处理服务器已启动，地址：' + host + '，端口:' + port);
+});
---
+++ src/app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Kane on 15/8/19.
+ */
+let express = require('express');
+let bodyParser = require('body-parser');
+let api = require('./api');
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/report', api);
+
+module.exports = app;
+
+let server = app.listen(9875, () => {
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.info('报告数据处理服务器已启动，地址：' + host + '，端口:' + port);
+});
